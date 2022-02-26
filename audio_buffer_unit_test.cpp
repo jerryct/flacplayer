89530e134295f7910d01b0dea036b2b557f0a46e
Jerry Crunchtime <jerry.c.t@web.de>
2022-02-26 21:02:42 +0100

    Rewrite AudioBuffer tests

--- audio_buffer_unit_test.cpp
+++ audio_buffer_unit_test.cpp
@@ -1,153 +1,198 @@
 // SPDX-License-Identifier: MIT
 
 #include "audio_buffer.h"
+#include <algorithm>
 #include <array>
 #include <cstring>
 #include <gtest/gtest.h>
+#include <numeric>
+#include <vector>
 
 namespace plac {
 namespace {
 
-class AudioBufferTest : public ::testing::Test {
-protected:
-  void SetUp() override {}
-  void TearDown() override {}
+struct Reader {
+  ssize_t operator()(const AudioFormat format, const u_char *data, const size_t count) {
+    if (do_error_injection_) {
+      return -23;
+    } else {
+      const auto channel_size = format.bits / 8;
+      const auto frame_size = channel_size * 2;
 
-  std::array<int, 12> left_{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-  std::array<int, 12> right_{12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-  AudioFormat format_{24, 2, 44100};
-};
-
-TEST_F(AudioBufferTest, Construct) {
-  AudioBuffer<24> buffer;
-  auto reader = [](AudioFormat, const u_char *, const size_t count) { return static_cast<ssize_t>(count); };
-
-  EXPECT_FLOAT_EQ(0.0F, buffer.GetFillLevel());
-  EXPECT_TRUE(buffer.IsEmpty());
-  EXPECT_EQ(0, buffer.Read(format_, 23, reader));
-}
-
-TEST_F(AudioBufferTest, Write) {
-  AudioBuffer<24> buffer;
-
-  EXPECT_EQ(1, buffer.Write(format_, left_.cbegin(), right_.cbegin(), 1));
-  EXPECT_FLOAT_EQ(0.25F, buffer.GetFillLevel());
-  EXPECT_FALSE(buffer.IsEmpty());
-
-  EXPECT_EQ(2, buffer.Write(format_, left_.cbegin(), right_.cbegin(), 2));
-  EXPECT_FLOAT_EQ(0.75F, buffer.GetFillLevel());
-  EXPECT_FALSE(buffer.IsEmpty());
-
-  EXPECT_EQ(1, buffer.Write(format_, left_.cbegin(), right_.cbegin(), 2));
-  EXPECT_FLOAT_EQ(1.0F, buffer.GetFillLevel());
-  EXPECT_FALSE(buffer.IsEmpty());
-}
-
-TEST_F(AudioBufferTest, Read) {
-  AudioBuffer<24> buffer;
-  auto reader = [](AudioFormat, const u_char *, const size_t count) { return static_cast<ssize_t>(count); };
-
-  EXPECT_EQ(4, buffer.Write(format_, left_.cbegin(), right_.cbegin(), 4));
-  EXPECT_FLOAT_EQ(1.0F, buffer.GetFillLevel());
-  EXPECT_FALSE(buffer.IsEmpty());
-
-  EXPECT_EQ(4, buffer.Read(format_, 4, reader));
-  EXPECT_FLOAT_EQ(0.0F, buffer.GetFillLevel());
-  EXPECT_TRUE(buffer.IsEmpty());
-
-  EXPECT_EQ(0, buffer.Read(format_, 4, reader));
-  EXPECT_FLOAT_EQ(0.0F, buffer.GetFillLevel());
-  EXPECT_TRUE(buffer.IsEmpty());
-}
-
-TEST_F(AudioBufferTest, ReadWhenWrapAround) {
-  AudioBuffer<24> buffer;
-  auto reader = [](AudioFormat, const u_char *, const size_t count) { return static_cast<ssize_t>(count); };
-
-  EXPECT_EQ(3, buffer.Write(format_, left_.cbegin(), right_.cbegin(), 3));
-  EXPECT_EQ(0, buffer.Drain(format_, reader));
-  EXPECT_EQ(4, buffer.Write(format_, left_.cbegin(), right_.cbegin(), 4));
-
-  EXPECT_EQ(1, buffer.Read(format_, 4, reader));
-}
-
-TEST_F(AudioBufferTest, ReadWhenError) {
-  AudioBuffer<24> buffer;
-  auto reader = [](AudioFormat, const u_char *, size_t) { return -23; };
-
-  EXPECT_EQ(3, buffer.Write(format_, left_.cbegin(), right_.cbegin(), 3));
-  EXPECT_FLOAT_EQ(0.75F, buffer.GetFillLevel());
-
-  EXPECT_EQ(-23, buffer.Read(format_, 3, reader));
-  EXPECT_FLOAT_EQ(0.75F, buffer.GetFillLevel());
-}
-
-TEST_F(AudioBufferTest, DrainWhenError) {
-  AudioBuffer<24> buffer;
-  auto reader = [](AudioFormat, const u_char *, size_t) { return -23; };
-
-  EXPECT_EQ(3, buffer.Write(format_, left_.cbegin(), right_.cbegin(), 3));
-  EXPECT_FLOAT_EQ(0.75F, buffer.GetFillLevel());
-
-  EXPECT_EQ(-23, buffer.Drain(format_, reader));
-  EXPECT_FLOAT_EQ(0.75F, buffer.GetFillLevel());
-}
-
-TEST_F(AudioBufferTest, WriteRead) {
-  AudioBuffer<24> buffer;
-  auto reader = [](AudioFormat, const u_char *, const size_t count) { return static_cast<ssize_t>(count); };
-
-  EXPECT_EQ(4, buffer.Write(format_, left_.cbegin(), right_.cbegin(), 4));
-  EXPECT_FLOAT_EQ(1.0F, buffer.GetFillLevel());
-  EXPECT_FALSE(buffer.IsEmpty());
-
-  EXPECT_EQ(0, buffer.Write(format_, left_.cbegin(), right_.cbegin(), 4));
-  EXPECT_FLOAT_EQ(1.0F, buffer.GetFillLevel());
-  EXPECT_FALSE(buffer.IsEmpty());
-
-  EXPECT_EQ(0, buffer.Read(format_, 6, reader));
-  EXPECT_FLOAT_EQ(1.0F, buffer.GetFillLevel());
-  EXPECT_FALSE(buffer.IsEmpty());
-
-  EXPECT_EQ(2, buffer.Read(format_, 2, reader));
-  EXPECT_FLOAT_EQ(0.5F, buffer.GetFillLevel());
-  EXPECT_FALSE(buffer.IsEmpty());
-
-  EXPECT_EQ(2, buffer.Write(format_, left_.cbegin(), right_.cbegin(), 4));
-  EXPECT_FLOAT_EQ(1.0F, buffer.GetFillLevel());
-  EXPECT_FALSE(buffer.IsEmpty());
-}
-
-TEST_F(AudioBufferTest, Recover) {
-  AudioBuffer<24> buffer;
-  std::vector<int> left_recovered;
-  std::vector<int> right_recovered;
-
-  auto reader = [&left_recovered, &right_recovered](const AudioFormat format, const u_char *const data,
-                                                    const size_t count) {
-    const auto channel_size = format.bits / 8;
-    const auto frame_size = channel_size * 2;
-
-    for (size_t i{0}; i < count; ++i) {
-      int left{};
-      std::memcpy(&left, &data[frame_size * i], channel_size);
-      left_recovered.push_back(left);
-      int right{};
-      std::memcpy(&right, &data[frame_size * i + channel_size], channel_size);
-      right_recovered.push_back(right);
+      for (size_t i{0}; i < count; ++i) {
+        int left{};
+        std::memcpy(&left, &data[frame_size * i], channel_size);
+        left_.push_back(left);
+        int right{};
+        std::memcpy(&right, &data[frame_size * i + channel_size], channel_size);
+        right_.push_back(right);
+      }
+      return static_cast<ssize_t>(count);
     }
-    return static_cast<ssize_t>(count);
   };
-
-  auto left = left_.cbegin();
-  auto right = right_.cbegin();
-  for (; (left != left_.cend()) || (right != right_.cend()); left += 3, right += 3) {
-    EXPECT_EQ(3, buffer.Write(format_, left, right, 3));
-    EXPECT_EQ(0, buffer.Drain(format_, reader));
+  void Flush() {
+    left_.clear();
+    right_.clear();
   }
 
-  EXPECT_TRUE(std::equal(left_.cbegin(), left_.cend(), left_recovered.cbegin(), left_recovered.cend()));
-  EXPECT_TRUE(std::equal(right_.cbegin(), right_.cend(), right_recovered.cbegin(), right_recovered.cend()));
+  bool do_error_injection_{false};
+  std::vector<int> left_{};
+  std::vector<int> right_{};
+};
+
+template <typename T> class AudioBufferTest : public ::testing::Test {
+protected:
+  void SetUp() override {
+    std::iota(left_.begin(), left_.end(), 1);
+    std::copy(left_.cbegin(), left_.cend(), right_.rbegin());
+  }
+  void TearDown() override {}
+
+  void FillBuffer() {
+    while (buffer_.GetFillLevel() < 1.0F) {
+      buffer_.Write(format_, left_.cbegin(), right_.cbegin(), 8);
+    }
+    ASSERT_FLOAT_EQ(1.0F, buffer_.GetFillLevel());
+  }
+
+  void FillBufferExceptLast() {
+    const size_t last{NumberOfSamplesPerStride() - 1};
+    for (size_t i{0}; i < last; ++i) {
+      buffer_.Write(format_, left_.cbegin(), right_.cbegin(), 8);
+    }
+    ASSERT_FLOAT_EQ(last / static_cast<float>(NumberOfSamplesPerStride()), buffer_.GetFillLevel());
+  }
+
+  // 16bps: 4 Bytes per sample, 192 Bytes have 48 samples, 5 write with 8 samples per call/stride
+  // 24bps: 6 Bytes per sample, 192 Bytes have 32 samples, 4 write with 8 samples per call/stride
+  // 32bps: 8 Bytes per sample, 192 Bytes have 24 samples, 3 write with 8 samples per call/stride
+  size_t NumberOfSamplesPerStride() const { return AsFrames(format_, 192) / 8; }
+
+  std::array<int, 16> left_;
+  std::array<int, 16> right_;
+  AudioFormat format_{T(), 2, 44100};
+  Reader reader_;
+
+  AudioBuffer<192> buffer_;
+};
+
+using MyTypes = ::testing::Types<std::integral_constant<unsigned, 16>, std::integral_constant<unsigned, 24>>;
+TYPED_TEST_SUITE(AudioBufferTest, MyTypes);
+
+TYPED_TEST(AudioBufferTest, Construct) {
+  EXPECT_FLOAT_EQ(0.0F, this->buffer_.GetFillLevel());
+  EXPECT_TRUE(this->buffer_.IsEmpty());
+}
+
+TYPED_TEST(AudioBufferTest, GetFillLevel) {
+  int i{1};
+  while (this->buffer_.GetFillLevel() < 1.0F) {
+    EXPECT_EQ(8, this->buffer_.Write(this->format_, this->left_.cbegin(), this->right_.cbegin(), 8));
+    EXPECT_FLOAT_EQ(i / static_cast<float>(this->NumberOfSamplesPerStride()), this->buffer_.GetFillLevel());
+    EXPECT_FALSE(this->buffer_.IsEmpty());
+    ++i;
+  }
+}
+
+TYPED_TEST(AudioBufferTest, Write) {
+  EXPECT_EQ(8, this->buffer_.Write(this->format_, this->left_.cbegin(), this->right_.cbegin(), 8));
+}
+
+TYPED_TEST(AudioBufferTest, WriteWhenFull) {
+  this->FillBuffer();
+
+  EXPECT_EQ(0, this->buffer_.Write(this->format_, this->left_.cbegin(), this->right_.cbegin(), 8));
+}
+
+TYPED_TEST(AudioBufferTest, WriteWhenTooManyFrames) {
+  this->FillBufferExceptLast();
+
+  EXPECT_EQ(8, this->buffer_.Write(this->format_, this->left_.cbegin(), this->right_.cbegin(), 16));
+}
+
+TYPED_TEST(AudioBufferTest, Read) {
+  EXPECT_EQ(8, this->buffer_.Write(this->format_, this->left_.cbegin(), this->right_.cbegin(), 8));
+
+  EXPECT_EQ(8, this->buffer_.Read(this->format_, 8, this->reader_));
+  EXPECT_EQ(this->reader_.left_, (std::vector<int>{1, 2, 3, 4, 5, 6, 7, 8}));
+  EXPECT_EQ(this->reader_.right_, (std::vector<int>{16, 15, 14, 13, 12, 11, 10, 9}));
+}
+
+TYPED_TEST(AudioBufferTest, ReadWhenEmpty) {
+  ASSERT_TRUE(this->buffer_.IsEmpty());
+
+  EXPECT_EQ(0, this->buffer_.Read(this->format_, 8, this->reader_));
+  EXPECT_EQ(0, this->reader_.left_.size());
+  EXPECT_EQ(0, this->reader_.right_.size());
+}
+
+TYPED_TEST(AudioBufferTest, ReadWhenTooManyFrames) {
+  EXPECT_EQ(8, this->buffer_.Write(this->format_, this->left_.cbegin(), this->right_.cbegin(), 8));
+
+  EXPECT_EQ(0, this->buffer_.Read(this->format_, 9, this->reader_));
+  EXPECT_EQ(0, this->reader_.left_.size());
+  EXPECT_EQ(0, this->reader_.right_.size());
+}
+
+TYPED_TEST(AudioBufferTest, ReadWhenWrapAroundWithoutRest) {
+  this->FillBuffer();
+  EXPECT_EQ(0, this->buffer_.Drain(this->format_, this->reader_));
+  ASSERT_TRUE(this->buffer_.IsEmpty());
+  this->reader_.Flush();
+  EXPECT_EQ(8, this->buffer_.Write(this->format_, this->left_.cbegin(), this->right_.cbegin(), 8));
+
+  EXPECT_EQ(8, this->buffer_.Read(this->format_, 8, this->reader_));
+  EXPECT_EQ(this->reader_.left_, (std::vector<int>{1, 2, 3, 4, 5, 6, 7, 8}));
+  EXPECT_EQ(this->reader_.right_, (std::vector<int>{16, 15, 14, 13, 12, 11, 10, 9}));
+}
+
+TYPED_TEST(AudioBufferTest, ReadWhenWrapAroundWithRest) {
+  this->FillBufferExceptLast();
+  EXPECT_EQ(0, this->buffer_.Drain(this->format_, this->reader_));
+  ASSERT_TRUE(this->buffer_.IsEmpty());
+  this->reader_.Flush();
+  EXPECT_EQ(16, this->buffer_.Write(this->format_, this->left_.cbegin(), this->right_.cbegin(), 16));
+
+  EXPECT_EQ(8, this->buffer_.Read(this->format_, 16, this->reader_));
+  EXPECT_EQ(8, this->buffer_.Read(this->format_, 8, this->reader_));
+  EXPECT_EQ(this->reader_.left_, (std::vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}));
+  EXPECT_EQ(this->reader_.right_, (std::vector<int>{16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1}));
+}
+
+TYPED_TEST(AudioBufferTest, ReadWhenError) {
+  this->reader_.do_error_injection_ = true;
+  this->FillBuffer();
+
+  EXPECT_EQ(-23, this->buffer_.Read(this->format_, 3, this->reader_));
+  EXPECT_EQ(0, this->reader_.left_.size());
+  EXPECT_EQ(0, this->reader_.right_.size());
+}
+
+TYPED_TEST(AudioBufferTest, Drain) {
+  EXPECT_EQ(16, this->buffer_.Write(this->format_, this->left_.cbegin(), this->right_.cbegin(), 16));
+
+  EXPECT_EQ(0, this->buffer_.Drain(this->format_, this->reader_));
+  EXPECT_TRUE(this->buffer_.IsEmpty());
+  EXPECT_EQ(this->reader_.left_, (std::vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}));
+  EXPECT_EQ(this->reader_.right_, (std::vector<int>{16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1}));
+}
+
+TYPED_TEST(AudioBufferTest, DrainWhenFull) {
+  this->FillBuffer();
+
+  EXPECT_EQ(0, this->buffer_.Drain(this->format_, this->reader_));
+  EXPECT_TRUE(this->buffer_.IsEmpty());
+  EXPECT_EQ(AsFrames(this->format_, 192), this->reader_.left_.size());
+  EXPECT_EQ(AsFrames(this->format_, 192), this->reader_.right_.size());
+}
+
+TYPED_TEST(AudioBufferTest, DrainWhenError) {
+  this->reader_.do_error_injection_ = true;
+  this->FillBuffer();
+
+  EXPECT_EQ(-23, this->buffer_.Drain(this->format_, this->reader_));
+  EXPECT_EQ(0, this->reader_.left_.size());
+  EXPECT_EQ(0, this->reader_.right_.size());
 }
 
 } // namespace
