6126e31c8668c619a33e53ea80e29fe480a6c7f4
Jerry Crunchtime <jerry.c.t@web.de>
2026-04-03 13:07:53 +0200

    Improve comments

--- linux_player.cpp
+++ linux_player.cpp
@@ -10,28 +10,22 @@ int main(int argc, char *argv[]) {
     cpu_set_t cpu_set;
     CPU_ZERO(&cpu_set);
     CPU_SET(3, &cpu_set);
-    if (0 != ::sched_setaffinity(0, sizeof(cpu_set), &cpu_set)) {
-        printf("Failed to set CPU affinity\n");
+    // to make affinity work,
+    // fence cpu `isolcpus=3 irqaffinity=0-2` in /boot/firmware/cmdline.txt
+    if (::sched_setaffinity(0, sizeof(cpu_set), &cpu_set) != 0) {
+        LOG_ERROR("failed to set CPU affinity");
     }
-    // https://wiki.linuxfoundation.org/realtime/documentation/howto/applications/application_base
 
-    // fence cpu
-    // isolcpus=3 irqaffinity=0-2
-    // in /boot/firmware/cmdline.txt
-    // check that preempt is enabled
-    // $ cat /sys/kernel/realtime
-    // expected value is 1
-    // /proc/sys/kernel/sched_rt_runtime_us (RT throttling) should be set to -1
-    // because it defines the time reserved for RT task per seconds.
-    // if the value is too small the task is not scheduled
-    // if EPERM is returned add capability for the file
-    // $ sudo setcap 'cap_sys_nice=eip' <path>/flacplayer
-    // check it with $ getcap <path>/flacplayer
+    // https://wiki.linuxfoundation.org/realtime/documentation/howto/applications/application_base
+    // check that PREEMPT_RT is enabled with `cat /sys/kernel/realtime`. expected value is 1
+
     sched_param param{};
     param.sched_priority = 60; // sched_get_priority_max(SCHED_FIFO);
-    const int r{::sched_setscheduler(0, SCHED_FIFO, &param)};
-    if (r != 0) {
-        std::cout << "failure: " << strerror(errno) << std::endl;
+    if (::sched_setscheduler(0, SCHED_FIFO, &param) != 0) {
+        // if EPERM is returned,
+        // add capability for the file with `sudo setcap 'cap_sys_nice=eip' <path>/flacplayer`
+        // check it with `getcap <path>/flacplayer`
+        LOG_ERROR("failed to set scheduling parameters: {}", ::strerror(errno));
     }
 
     ::plac::Stream stream{plac::AlsaAudioDevice::Output::uln2};
